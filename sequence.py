4306f5c89d146703ea7a54e4e71665e553481dcf
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-18 21:10:18 +0530

    konwing sequence datatypes

---
+++ sequence.py
@@ -0,0 +1,15 @@
+restaurant=["tables","chairs","workers","32"]
+print(restaurant)
+print(type(restaurant))
+
+print("")
+
+restaurant=("tables","chairs","workers","32")
+print(restaurant)
+print(type(restaurant))
+
+print("")
+
+for x in range(20):
+          print(x)
+print(type(x))
